a08fb1cbcd849a627e9b05956ceaccc1a9b023cb
Sudipta Mondal <sudiptamondal05@gmail.com>
2020-11-04 14:59:47 +0530

    updated cv_data

--- cv_data.js
+++ cv_data.js
@@ -1,145 +1,200 @@
-var cv_data = {
-    "years": ["2012","2013","2014","2015","2016","2017","2018","2019","2020"],
-    "items": {
-        "2012": {
-            "position":"Software Developer",
-            "roles_played":[
-                "Web Developer"
-            ],
-            "other_roles_played": [                "API Developer - REST"],
-            "components": [
-                "Pulsemob - Event Management Web Application",
-                "Airgol - Live Stream video from Android/iPhone"
-            ],
-            "impact": []
-        },
-        "2013": {
-            "position":"Software Developer/Freelancer",
-            "roles_played": [
-                "Full Stack Developer"
-            ],
-            "other_roles_played":[
-                "Joined Infosys as Systems Engineer Trainee in Aug, 2013"
-            ],
-            "components":[
-                "Artface - Website for Aucitoning Paintings",
-                "RMLC - Internal Application for Robertson Mailing List Company for OM/PO/Report",
-                "CLP - Christian Light Publication Front End website development",
-                "Oracle E Biz Suite - Basic Training"
-            ],
-            "impact": []
-        },
-        "2014": {
-            "position": "",
-            "roles_played":[""],
-            "other_roles_played":[""],
-            "components":[""],
-            "impact": []
-        },
-        "2015": {
-            "position": "",
-            "roles_played":[""],
-            "other_roles_played":[""],
-            "components":[""],
-            "impact": []
-        },
-        "2016": {
-            "position": "",
-            "roles_played":[""],
-            "other_roles_played":[""],
-            "components":[""],
-            "impact": []
-        },
-        "2017": {
-            "position": "",
-            "roles_played":[""],
-            "other_roles_played":[""],
-            "components":[""],
-            "impact": []
-        },
-        "2018": {
-            "position": "Technical Leader",
-            "roles_played": [
-                "Product Hub Implementation Consultant",
-                "PDH Configuration and Customization",
-                "CRP Co-Ordinator",
-                "Testing Co-ordinator"
-            ],
-            "other_roles_played": [
-                "RFP Estimations",
-                "Fit-Gap Analysis",
-                "Business Requirement POCs"
-            ],
-            "components":[
-                "Product Data Model",
-                "New Item Workflows",
-                "Change Order Workflows",
-                "Spoke Systems (Import and Publication)",
-                "OBIEE and OTBI Reporting",
-                "Custom Objects, Page Customizations, Groovy Scripting"
-            ],
-            "impact":[
-                "Co-ordinated between Client and Vendors for Project QA Testing",
-                "Lead Offshore Development for PH Cloud Implementation",
-                "Supported Critical UAT phase for Shwerwin Williams PH Cloud Implementation"
-            ]
-        },
-        "2019" : {
-            "position": "Lead Consultant",
-            "roles_played": [
-                "Product Hub Implementation Consultant",
-                "PDH Configuration and Customization",
-                "CRP / UAT Co-Ordinator",
-                "Integration Lead"
-            ],
-            "other_roles_played": [
-                "RFP Estimations",
-                "Fit-Gap Analysis",
-                "Business Requirement POCs"
-            ],
-            "components":[
-                "Product Data Model",
-                "New Item Workflows",
-                "Change Order Workflows",
-                "Spoke Systems (Import and Publication)",
-                "OBIEE and OTBI Reporting",
-                "Custom Objects, Page Customizations, Groovy Scripting"
-            ],
-            "impact":[
-                "Co-ordinated between Client and Vendors for Project QA Testing",
-                "Lead Offshore Development for PH Cloud Implementation",
-                "Supported Critical UAT phase for Shwerwin Williams PH Cloud Implementation"
-            ]
-        },
-        "2020" : {
-            "position": "Lead Consultant",
-            "roles_played": [
-                "Product Hub Implementation Consultant",
-                "Testing and QA Lead",
-                "Implementation Team champion to Triage all defects",
-                "with Business raised during Course of implementation",
-                "Defect Analysis and Resolutions",
-                "Liaison with Oracle CSM/TAM for Customer Success"
-            ],
-            "other_roles_played": [
-                "RFP Estimations",
-                "Fit-Gap Analysis",
-                "Business Requirement POCs"
-            ],
-            "components":[
-                "Product Data Model",
-                "New Item Workflows",
-                "Change Order Workflows",
-                "Spoke Systems (Import and Publication)",
-                "OBIEE and OTBI Reporting",
-                "Custom Objects, Page Customizations, Groovy Scripting"
-            ],
-            "impact":[
-                "Co-ordinated between Client and Vendors for Project QA Testing",
-                "Took additional role of integration lead after onsite was rolled off",
-                "Developed Data Upload utilities for Oracle Cloud which helped reduce setup effort from Days to Hours for multiple clients",
-                "Created Employee Creation Utility which is used across Projects. Demo scheduled with Oracle to put on Oracle Marketplace"
-            ]
-        },
+    var cv_data = {
+  "years": [
+    "2012",
+    "2013",
+    "2014",
+    "2015",
+    "2016",
+    "2017",
+    "2018",
+    "2019",
+    "2020"
+  ],
+  "items": {
+    "2012": {
+      "position": "Software Developer",
+      "roles_played": [
+        "Web Developer"
+      ],
+      "other_roles_played": [
+        "API Developer - REST"
+      ],
+      "components": [
+        "Pulsemob - Event Management Web Application",
+        "Airgol - Live Stream video from Android/iPhone"
+      ],
+      "impact": []
+    },
+    "2013": {
+      "position": "Software Developer/Freelancer",
+      "roles_played": [
+        "Full Stack Developer"
+      ],
+      "other_roles_played": [
+        "Joined Infosys as Systems Engineer Trainee in Aug, 2013"
+      ],
+      "components": [
+        "Artface - Website for Aucitoning Paintings",
+        "RMLC - Internal Application for Robertson Mailing List Company for OM/PO/Report",
+        "CLP - Christian Light Publication Front End website development",
+        "Oracle E Biz Suite - Basic Training"
+      ],
+      "impact": []
+    },
+    "2014": {
+      "position": "Systems Engineer",
+      "roles_played": [
+        "Technical Developer"
+      ],
+      "other_roles_played": [
+		
+      ],
+      "components": [
+        "",
+		"Technical and Functional Specification design documentation"
+      ],
+      "impact": []
+    },
+    "2015": {
+      "position": "",
+      "roles_played": [
+        "Technical Developer"
+      ],
+      "other_roles_played": [
+        "Technical Change Management QA"
+      ],
+      "components": [
+        ""
+      ],
+      "impact": []
+    },
+    "2016": {
+      "position": "Technology Analyst",
+      "roles_played": [
+        "Oracle Consultant for Client BI Technical Team",
+        "IT Support Member for Customer's Europe Operations"
+      ],
+      "other_roles_played": [
+        "Lead Technical Design changes for O2C Implementation",
+        "Handle Data migration activities for Sales Offices going live",
+        "Manage SSRS/BI Report development, documentation and Co-ordinate testing and Migration to Live Server"
+      ],
+      "components": [
+        "Trade/Vendor reports",
+        "ERS Reporting",
+        "Instance Setup for AR/AP/INV/PO",
+        "Item Validation Dashboard",
+        "Quote Approval via AME",
+        "Forms Personalization - AP Invoice Workbench",
+        "GL Statutory Reports for France HQ"
+      ],
+      "impact": [
+        "Leading all Development/Migration activities which including Financial Data",
+        "Learned MSBI quickly to support client team with urgent requests"
+      ]
+    },
+    "2017": {
+      "position": "Technology Analyst",
+      "roles_played": [
+        "Oracle R12 Upgrade support member",
+        "Oracle Consultant for Client BI Technical Team"
+      ],
+      "other_roles_played": [
+        "Co-ordinate testing for Sites live with O2C",
+        "Upgrade related development activities. Modules- OM/PO",
+        "Upgrade related issue resolutions. Modules – AP/AR/OM/PO"
+      ],
+      "components": [
+        "Sales Order & Notes Import",
+        "PO/GL Reports",
+        "JSP Modifications for R12 Upgrade",
+        "PLSQL Modifications for R12 Upgrade"
+      ],
+      "impact": [
+        "Took additional role with helping Customer for R12 Upgrade along with regular project",
+        "Work with customers across Europe during the migration period from 11i to R12"
+      ]
+    },
+    "2018": {
+      "position": "Technical Leader",
+      "roles_played": [
+        "Product Hub Implementation Consultant",
+        "PDH Configuration and Customization",
+        "CRP Co-Ordinator",
+        "Testing Co-ordinator"
+      ],
+      "other_roles_played": [
+        "Business Requirement POCs"
+      ],
+      "components": [
+        "Product Data Model",
+        "New Item Workflows",
+        "Change Order Workflows",
+        "Spoke Systems (Import and Publication)",
+        "OBIEE and OTBI Reporting",
+        "Custom Objects, Page Customizations, Groovy Scripting"
+      ],
+      "impact": [
+        "Implemented out of scope SOA Web Services which helped client with Trading Partner inactivation"
+      ]
+    },
+    "2019": {
+      "position": "Lead Consultant",
+      "roles_played": [
+        "Product Hub Implementation Consultant",
+        "PDH Configuration and Customization",
+        "CRP / UAT Co-Ordinator",
+        "Integration Lead"
+      ],
+      "other_roles_played": [
+        "RFP Estimations",
+        "Fit-Gap Analysis",
+        "Business Requirement POCs"
+      ],
+      "components": [
+        "Product Data Model",
+        "New Item Workflows",
+        "Change Order Workflows",
+        "Spoke Systems (Import and Publication)",
+        "OBIEE and OTBI Reporting",
+        "Custom Objects, Page Customizations, Groovy Scripting"
+      ],
+      "impact": [
+        "Co-ordinated between Client and Vendors for Project QA Testing",
+        "Lead Offshore Development for PH Cloud Implementation",
+        "Supported Critical UAT phase for Shwerwin Williams PH Cloud Implementation"
+      ]
+    },
+    "2020": {
+      "position": "Lead Consultant",
+      "roles_played": [
+        "Product Hub Implementation Consultant",
+        "Testing and QA Lead",
+        "Implementation Team champion to Triage all defects",
+        "with Business raised during Course of implementation",
+        "Defect Analysis and Resolutions",
+        "Liaison with Oracle CSM/TAM for Customer Success"
+      ],
+      "other_roles_played": [
+        "RFP Estimations",
+        "Fit-Gap Analysis",
+        "Business Requirement POCs"
+      ],
+      "components": [
+        "Product Data Model",
+        "New Item Workflows",
+        "Change Order Workflows",
+        "Spoke Systems (Import and Publication)",
+        "OBIEE and OTBI Reporting",
+        "Custom Objects, Page Customizations, Groovy Scripting",
+        "User/Employee Creation BOT"
+      ],
+      "impact": [
+        "Co-ordinated between Client and Vendors for Project QA Testing",
+        "Took additional role of integration lead after onsite was rolled off",
+        "Developed Data Upload utilities for Oracle Cloud which helped reduce setup effort from Days to Hours for multiple clients",
+        "Created Employee Creation Utility which is used across Projects. Demo scheduled with Oracle to put on Oracle Marketplace"
+      ]
     }
+  }
 }
